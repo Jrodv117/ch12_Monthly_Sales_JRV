58eae7d44c82bac019efffce924b9a66e2e49dd5
jrodr117 <jrodr113@msudenver.edu>
2022-10-23 17:40:21 -0600

    added code to write to txt file and an edit function that is about to be tested

--- ch12_Monthly_Sales_JRV.py
+++ ch12_Monthly_Sales_JRV.py
@@ -5,6 +5,9 @@ with open(FILE_NAME) as f:
     for line in f:
         (key,val) = line.split()
         monthly_sales_dictionary[(key)] = int(val)
+        
+with open(FILE_NAME,'w', encoding='utf-8') as f:
+    f.write(monthly_sales_dictionary)
 
 def menu():
     print('Command Menu')
@@ -15,8 +18,13 @@ def menu():
         
 def view(dictionary):
     month = input('Three letter month: ')
-    print(f'Sales for month: {dictionary.get(month)}')
+    print(f'Sales amount for {month} is {dictionary.get(month)}')
     
+def edit(dictionary):
+        month = input('\nThree letter month: ')
+        amount = int(input('Sales amount: '))
+        dictionary[month] = amount
+        
 def main():
     print('Montly Sales Program\n')
     menu()
@@ -24,5 +32,6 @@ def main():
     while command != 'exit':
         if command == 'view':
             view(monthly_sales_dictionary)
-    
+        elif command == edit:
+            edit(monthly_sales_dictionary)
 main()
