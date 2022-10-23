9f26f91904acf0981174668ba2ae2108cc6286df
jrodr117 <jrodr113@msudenver.edu>
2022-10-23 15:54:56 -0600

    using f instead of sales to follow convention

--- ch12_Monthly_Sales_JRV.py
+++ ch12_Monthly_Sales_JRV.py
@@ -1,8 +1,8 @@
 
 monthly_sales_dictionary = {}
 FILE_NAME = 'monthly_sales.txt'
-with open(FILE_NAME) as sales:
-    for line in sales:
+with open(FILE_NAME) as f:
+    for line in f:
         (key,val) = line.split()
         monthly_sales_dictionary[(key)] = int(val)
         
