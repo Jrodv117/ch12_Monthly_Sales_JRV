3d916988a2fa9ab79cf3786ab69ddf6f09ec0d3b
jrodr117 <jrodr113@msudenver.edu>
2022-10-23 16:07:38 -0600

    added menu function that prints the menu

--- ch12_Monthly_Sales_JRV.py
+++ ch12_Monthly_Sales_JRV.py
@@ -5,9 +5,14 @@ with open(FILE_NAME) as f:
     for line in f:
         (key,val) = line.split()
         monthly_sales_dictionary[(key)] = int(val)
+
+def menu():
+    print('Command Menu')
+    print('view - View sales for specified month')
+    print('edit - Edit sales for specified month')
+    print('totals - View sales summary for year')
+    print('exit - Exit program')
         
-        
-print(monthly_sales_dictionary)
 
 def main():
     print('Montly Sales Program\n')
