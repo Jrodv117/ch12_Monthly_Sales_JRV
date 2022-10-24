45648410d591c274da03ee35e8ff7b9b36b041d8
jrodr117 <jrodr113@msudenver.edu>
2022-10-23 20:52:07 -0600

    added a function to check for month in the dictionary

--- ch12_Monthly_Sales_JRV.py
+++ ch12_Monthly_Sales_JRV.py
@@ -1,10 +1,14 @@
+from operator import truediv
+
+
 monthly_sales_dictionary = {}
 
 FILE_NAME = 'monthly_sales.txt'
-with open(FILE_NAME) as f:
-    for line in f:
-        (key,val) = line.split()
-        monthly_sales_dictionary[(key)] = int(val)
+def read_text():
+    with open(FILE_NAME) as f:
+        for line in f:
+            (key,val) = line.split()
+            monthly_sales_dictionary[(key)] = int(val)
         
 def write_to_txt(dictionary):        
     with open(FILE_NAME,'w', encoding='utf-8') as f:
@@ -13,7 +17,7 @@ def write_to_txt(dictionary):
         f.close() 
 
 
-def menu():
+def menus():
     print('Command Menu')
     print('view - View sales for specified month')
     print('edit - Edit sales for specified month')
@@ -21,25 +25,45 @@ def menu():
     print('exit - Exit program')
         
 def view(dictionary):
-    month = input('Three letter month: ')
-    print(f'Sales amount for {month} is {dictionary.get(month)}')
+    month = input('Three letter month: ').capitalize()
+    checked_input = check_entry(dictionary, month)
+    if checked_input == True:
+        print(f'Sales amount for {month} is {dictionary.get(month)}')
+    else:
+        pass
     
 def edit(dictionary):
-        month = input('\nThree letter month: ')
+    month = input('\nThree letter month: ').capitalize()
+    checked_input = check_entry(dictionary, month)
+    if checked_input == True:
         amount = int(input('Sales amount: '))
         dictionary[month] = amount
         write_to_txt(dictionary)
+    else: 
+        pass
 
 def totals(dictionary):
     sum_of_values = sum(dictionary.values())
     print(f'Yearly total: {sum_of_values}')
-    print(f'Monthly average: {sum_of_values/len(dictionary)}')
-        
+    print(f'Monthly average: {sum_of_values/len(dictionary)}') 
+    
+def check_entry(dictionary, month):
+    check_input = True
+    if month in dictionary:
+        pass
+    else:
+        check_input = False
+        print('Invalid three-letter month')
+    return check_input
+    
+
+
 def main():
     print('Montly Sales Program\n')
-    menu()
+    read_text()
+    menus()
     while True:
-        command = input('\nCommand: ').lower
+        command = input('\nCommand: ').lower()
         if command == 'view':
             view(monthly_sales_dictionary)
         elif command == 'edit':
